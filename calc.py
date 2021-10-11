2626510905ce4cfe6404b85bd2db16eed44fb1b2
Nathan Dalton <nathan_dalton@outlook.com>
2021-10-11 12:34:43 +1100

    update

--- calc.py
+++ calc.py
@@ -1,14 +1,14 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
+    num0, num1 = int(num0), int(num1)
     if operator == '+':
         return num0 + num1
     elif operator == '-':
         return num0 - num1
+    elif operator == '*':
+        return num0 * num1
+    elif operator == '/':
+        return num0 / num1
     else:
         print('unknown operator!')
         return None
-#test
-#test2
-#test3
-#test4
-#test6
